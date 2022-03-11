bf140c20fb1da5769bdf502a69dc0cd0e4cd0976
jeong-seon <wkjj97@naver.com>
2022-03-11 16:38:23 +0900

    day_12_commit!

---
+++ start/src/day12/MyWin.java
@@ -0,0 +1,79 @@
+package day12;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyWin {
+	JFrame fr;
+	JButton btn1, btn2;
+	JPanel mainP, bPan;
+	
+	public MyWin() {
+		fr = new JFrame();
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainP = new JPanel();
+		mainP.setPreferredSize(new Dimension(280, 450));
+		mainP.setBackground(Color.GREEN);
+		
+		bPan = new JPanel();
+		bPan.setLayout(new GridLayout(1, 2));
+		
+		btn1 = new JButton("닫기");
+		btn2 = new JButton("변경");
+		
+
+		
+		/*
+		// 닫기 이벤트 추가 (재사용을 할 경우)
+		ActionListener act = new ActionListener() {
+			// 이 클래스는 무명 내부클래스...
+			// 오버라이드하는 함수의 내용이 극도로 짧아서
+			// 별도로 클래스를 구현하는 것이 오히려 더 번거롭다.
+			// 따라서 구현하는 순간 new 시킨 인스턴스를 사용하면 된다.
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		};
+		*/
+		
+		ActionListener act = new MyEvt();
+		
+		// 닫기 이벤트 추가
+		btn1.addActionListener(act);
+//		btn2.addActionListener(act);
+		
+		btn2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mainP.setBackground(Color.blue);
+			}
+		});
+		
+		bPan.add(btn1);
+		bPan.add(btn2);
+		
+		fr.add(mainP, BorderLayout.CENTER);
+		fr.add(bPan, BorderLayout.SOUTH);
+		
+		fr.setVisible(true);
+		fr.setSize(300, 500);
+		fr.setResizable(false);
+		
+		
+	}
+	public static void main(String[] args) {
+		new MyWin();
+	}
+
+	class MyEvt implements ActionListener {
+		// 전역 내부 클래스...
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	}
+	
+}
---
+++ start/src/day12/sub/Ex01.java
@@ -0,0 +1,21 @@
+package day12.sub;
+
+/*
+	랜덤하게 0 ~ 2 정수를 발생시켜서
+		0 이 나오면 삼각형
+		1 가 나오면 사각형
+		2 가 나오면 원
+	의 형태로 10개의 도형을 만들어서
+	하나의 변수로 관리하고
+	각 도형의 내용을 출력하세요.
+*/
+public class Ex01 {
+
+	public Ex01() {
+		
+	}
+	public static void main(String[] args) {
+		new Ex01();
+	}
+
+}
