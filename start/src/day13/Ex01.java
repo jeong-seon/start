df85945af3ed8203def7e2c7ad11acd174cfe7b8
jeong-seon <wkjj97@naver.com>
2022-03-17 10:10:08 +0900

    commit

--- start/src/day13/Ex01.java
+++ start/src/day13/Ex01.java
@@ -10,17 +10,19 @@ package day13;
 		1.	배열의 범위를 벗어나서 사용하는 경우 (인덱스)
 		2.	숫자로 변환이 불가능한 경우
 		3.	평균을 계산하는데 0 으로 나누는 경우
+		4.	위의 세가지 이외의 경우...
  */
 public class Ex01 {
-	String[] arr = {"60", "75", "82", "73", "85", "100", "95", "9삼2"}; 
+	String[] arr = {"6사0", "75", "82", "73", "85", "100", "95", "9삼2", "87", "67"}; 
 
 	public Ex01() {
 		int sum = 0;
 		double avg = 0;
 		int len = arr.length;
+		int tmp = len;
 		
 		System.out.print("arr[] : ");
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 0; i < len; i++) {
 			System.out.print(arr[i] + "\t");
 		}
 		System.out.println();
@@ -30,7 +32,7 @@ public class Ex01 {
 			try {
 				int no = Integer.parseInt(arr[i]);
 				sum += no;
-				avg = sum / (double)len;
+				avg = sum / (double)tmp;
 				
 			} catch(ArithmeticException e) {
 				System.out.println("## 예외 발생 ## 정수를 0 으로 나누었습니다 !");
@@ -38,14 +40,17 @@ public class Ex01 {
 			} catch(IndexOutOfBoundsException e) {
 				System.out.println("## 예외 발생 ## 배열의 범위를 벗어났습니다 !");
 				
-			} catch(Exception e) {
+			} catch(NumberFormatException e) {
+				tmp -= 1;
 				System.out.println("## 예외 발생 ## " + arr[i] + " : 이 문자열은 숫자로 변환이 불가능합니다 !");
 				
+			} catch(Exception e) {
+				System.out.println("!! ## 나머지 예외 발생 ## !!");
 			}
 			
 		}
 		System.out.println("arr[] 의 합계 : " + sum);
-		System.out.println("arr[] 의 평균 : " + avg);
+		System.out.printf("arr[] 의 평균 : %.2f", avg);
 		
 	}
 
